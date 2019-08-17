08b723ab0d26ef8969dd3cb758d83567cfec93a8
Mohd Hafizuddin Kamilin <34746052+hafiz-kamilin@users.noreply.github.com>
2019-08-18 00:37:18 +0900

    Update smartfarm_problem.py

--- smartfarm_problem.py
+++ smartfarm_problem.py
@@ -5,7 +5,7 @@
 [Smart Tomato Farm Problem]
 
     Tomato farm is divided into 10 areas which have it own exclusive sprinkler (1 area 1 sprinkler). We
-    assume the sprinkler can freely adjust the irrigation area based on the sprinkler head. now
+    assume the sprinkler can freely adjust the irrigation area based on the sprinkler head. Now
     the problem is the water tank can only support 40 L/min of total irrigation output.
     In other words we can't execute the all sprinklers at the same time...
 
@@ -25,45 +25,47 @@
 
     From the hard coded sprinklers selection, we will get;
 
-        ROUND 1: [('sprinkler_01', 10, 5), ('sprinkler_02', 10, 5), ('sprinkler_07', 9, 2), ('sprinkler_16', 10, 5)]
-        ROUND 2: [('sprinkler_05', 13, 4), ('sprinkler_11', 13, 3), ('sprinkler_19', 13, 6)]
-        ROUND 3: [('sprinkler_03', 20, 6), ('sprinkler_20', 18, 3)]
-        ROUND 4: [('sprinkler_15', 15, 3)]
+        Aggregated sequence:
 
-    which mean, this program choose the best value versus specification we can get and at the same time
-    it arrange the sprinklers order of execution to abide the water tank output specification.
+        Step 1: [('sprinkler_01', 10, 5), ('sprinkler_02', 10, 5), ('sprinkler_07', 9, 2), ('sprinkler_16', 10, 5)]
+        Step 2: [('sprinkler_05', 13, 4), ('sprinkler_11', 13, 3), ('sprinkler_19', 13, 6)]
+        Step 3: [('sprinkler_03', 20, 6), ('sprinkler_20', 18, 3)]
+        Step 4: [('sprinkler_15', 15, 3)]
+
+    which mean, this program choose the best specification versus value we can get and at the same time
+    it arrange the sprinklers order of execution step to abide the water tank output limitation.
 
 """
 # cache to memoize (memoization) the result 
 cache = {}
 
-# function to calculate the total value for the carried actuators
-def total_value(actuators, specification_limit):
+# function to calculate the total value for the listed devices
+def total_value(listed_devices, system_cap):
 
-    # return the total value for the carried actuators
-    return  sum([x[2] for x in actuators]) if sum([x[1] for x in actuators]) < specification_limit else 0
+    # return the total value for the listed devices
+    return  sum([x[2] for x in listed_devices]) if sum([x[1] for x in listed_devices]) < system_cap else 0
 
 # function to perform the recursive dynamic programming algorithm to get the result
-def solve(actuators, specification_limit):
+def solve_knapsack(listed_devices, system_cap):
 
-    # if there is no more actuators
-    if not actuators:
+    # if there is no more listed devices
+    if not listed_devices:
         
         # return empty tuple
         return ()
 
-    # if the actuators and max weight are not in cache
-    if (actuators, specification_limit) not in cache:
+    # if the listed devices and system cap are not in cache
+    if (listed_devices, system_cap) not in cache:
 
         # load the first item into head
-        head = actuators[0]
-        # load the rest of actuators into tail
-        tail = actuators[1:]
+        head = listed_devices[0]
+        # load the rest of listed devices into tail
+        tail = listed_devices[1:]
         # TODO: supplementary study https://www.youtube.com/watch?v=xOlhR_2QCXY
-        include = (head,) + solve(tail, specification_limit - head[1])
-        dont_include = solve(tail, specification_limit)
+        include = (head,) + solve_knapsack(tail, system_cap - head[1])
+        dont_include = solve_knapsack(tail, system_cap)
         # if the total value and weight for the head is bigger than total value and weight for the tail 
-        if total_value(include, specification_limit) > total_value(dont_include, specification_limit):
+        if total_value(include, system_cap) > total_value(dont_include, system_cap):
 
             # the answer is head
             answer = include
@@ -74,16 +76,72 @@ def solve(actuators, specification_limit):
             answer = dont_include
         
         # load the answer into the cache
-        cache[(actuators, specification_limit)] = answer
+        cache[(listed_devices, system_cap)] = answer
     
     # return the cache
-    return cache[(actuators, specification_limit)]
+    return cache[(listed_devices, system_cap)]
+
+# function to sequence the choosen devices based on the system cap
+def sequencing_order(listed_devices, required_devices, system_cap):
+
+    # initialize counter
+    counter = 0
+    # initialize output
+    output = []
+
+    # as long there is remaining devices
+    while (required_devices != 0):
+
+        print ("\nKnapsack solution %s\n" %(counter + 1))
+        # compute the result
+        result = list(solve_knapsack(listed_devices, system_cap))
+        # show the list of device that need to be carried
+        print ("Selected device:")
+
+        # for every devices listed in the result
+        for x in result:
+
+            # as long there is remaining required devices
+            if (required_devices != 0):
+
+                # decrease by 1
+                required_devices -= 1
+                # show only the item name
+                print (" → " + x[0])
+
+            else:
+
+                # remove the excess element
+                result.remove(x)
+
+        # save the result into the output
+        output.insert(counter, result)
+        # compute the total value
+        print ("Value:", total_value(output[counter], system_cap))
+        # compute the total output
+        print ("Output:", sum([x[1] for x in output[counter]]))
+        # convert the tuple into a list
+        listed_devices = list(listed_devices)
+
+        # for every actuator listed in the output
+        for x in output[counter]:
+
+            # remove it from the listed devices
+            listed_devices.remove(x)
+
+        # convert the list back into a tuple
+        listed_devices = tuple(listed_devices)
+        # increase the counter
+        counter += 1
+
+    # return the output
+    return output
 
 # initiator
 if __name__ == '__main__':
 
     # actuator limit
-    specification_limit = 40
+    watertank_output = 40
     # number of farm partition
     farm_partition = 10
 
@@ -105,58 +163,15 @@ if __name__ == '__main__':
 
     # initialize counter
     counter = 0
-    # initialize solution
-    solution = []
-
-    # as long there is remaining partition
-    while (farm_partition != 0):
-
-        print ("\nROUND %s\n" %(counter + 1))
-        # compute the result
-        result = list(solve(actuators, specification_limit))
-        # show the list of actuators that need to be carried
-        print ("Selected actuators:")
-
-        # for every actuator listed in the result
-        for x in result:
-
-            # as long there is remaining partition
-            if (farm_partition != 0):
-
-                # decrease by 1
-                farm_partition -= 1
-                # show only the item name
-                print ("→ " + x[0])
-
-            else:
-
-                # remove the excess element
-                result.remove(x)
-
-        # save the result into a solution
-        solution.insert(counter, result)
-        # compute the total value
-        print ("value:", total_value(solution[counter], specification_limit))
-        # compute the total output
-        print ("output:", sum([x[1] for x in solution[counter]]))
-        # convert the tuple into a list
-        actuators = list(actuators)
-
-        # for every actuator listed in the solution
-        for x in solution[counter]:
-
-            # remove it from the listed actuators
-            actuators.remove(x)
-
-        # convert the list back into a tuple
-        actuators = tuple(actuators)
-        # increase the counter
-        counter += 1
-
-    print ("\nAggregated solution:\n")
+    # compute the solution
+    solution = sequencing_order(actuators, farm_partition, watertank_output)
+    # show the solution in aggregated way
+    print ("\nAggregated sequence:\n")
 
     # for every actuator listed in the solution
     for x in solution:
 
-        # remove it from the listed actuators
-        print (x)
+        # print it for every round
+        print ("Step %i: %s" %(counter + 1, x))
+        # increase the counter
+        counter += 1
